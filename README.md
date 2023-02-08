fbae1e88ed1c6fd1392eee5f281b19a35b883589
Huan-Yi Shen <hy2shen@uwaterloo.ca>
2023-02-08 00:57:17 -0500

    hornersRule

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # numericalMethod
 implement various classic numerical algorithms 
+
+This repository is inspired by the course ECE204 from the University of Waterloo. The organization will closely follow the ECE204 course taught by Lecturer D.W. Harder
+
+Approximation of the value of mathematic expressions 
+Approximation of solutions to algebric equations
+Approximation of solutions to analytic equations
+
+## Getting Started
+$ python -m venv venv (first venv indicate the venv module, second venv is the name of your virtual environment, can be anything)
+$ venv\Scripts\activate.bat
+$ pip install -r requirements.txt
+$ deactivate (to exit the venv)
+
+export new requirements
+$ pip freeze > requirements.txt
---
+++ hornersRule.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+# representing polynomial 
+# ax^n + bx^(n-1) + cx^(n-2) becomes [c, b, a]
+
+# helper functions
+def printPolynomial(coefficients):
+    currDeg = int(coefficients.size-1)        
+    print("The polynomial expression is --> ", end="")
+    for _ in range(coefficients.size-2):
+        print("{}(x^{}) + ".format(coefficients[currDeg], currDeg), end="")
+        currDeg -= 1
+    print("{}(x) + ".format(coefficients[1]), end="")
+    print("{}\n".format(coefficients[0]))
+
+
+def power_of_n(x, n):
+    res = 1
+    for i in range(n):
+        res *= x
+    return res
+
+
+def polynomialExpr(coefficients, x_var):
+    x_var = float(x_var)
+    currDeg = int(coefficients.size-1)
+    resStr = ""
+    for _ in range(coefficients.size-2):
+        resStr += "{}({}) + ".format(coefficients[currDeg], power_of_n(x_var, currDeg))
+        currDeg -= 1
+    resStr += "{}({}) + ".format(coefficients[1], x_var)
+    resStr += "{}".format(coefficients[0])
+    return resStr
+
+
+def evaluatePolynomial_navie(coeffs, x):
+    res = 0
+    for deg, coeff in enumerate(coeffs):
+        res += coeff * power_of_n(x, int(deg))
+    return res
+
+
+def evaluatePolynomial_horner(coeffs, x):
+    # ax^3+bx^2+cx+d = ((ax+b)*x+c)*x+d 
+    # where first iteration is ax+b, second is (...)*x+c
+    currDeg = int(coeffs.size-1)
+    res = coeffs[currDeg]
+    for _ in range(coeffs.size-1):
+        currDeg -= 1
+        res = res*x + coeffs[currDeg]
+    return res
+
+##################################################################
+# getting polynomial from command line        
+deg = int(input("degree of the polynomial: "))
+polynomial_coefficients = np.zeros(deg + 1)
+for i in range(polynomial_coefficients.size):
+    polynomial_coefficients[i] = input("enter coefficient for degree {}: ".format(i))
+
+printPolynomial(polynomial_coefficients)
+
+x_val = float(input("evaluate above expression at x="))
+EvalStr = polynomialExpr(polynomial_coefficients, x_val)
+print("{} = {}".format(EvalStr, evaluatePolynomial_navie(polynomial_coefficients, x_val)))
+print("{} = {}".format(EvalStr, evaluatePolynomial_horner(polynomial_coefficients, x_val)))
